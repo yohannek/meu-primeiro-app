5c1d495f7b6846cd122326005d1023cab36511f5
Yohanne Karine <yohannekarinek@gmail.com>
2026-08-17 10:39:00 -0300

    validações

--- src/app/features/checkout/checkout/checkout.ts
+++ src/app/features/checkout/checkout/checkout.ts
@@ -1,5 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms';
+import {
+    ReactiveFormsModule,
+    FormGroup,
+    FormControl,
+    Validators,
+    AbstractControl,
+    ValidationErrors,
+  } from '@angular/forms';
+
 import { CarrinhoService } from '../../../core/services/carrinho.service';
 
 @Component({
@@ -12,13 +20,34 @@ export class Checkout {
     carrinhoService = inject(CarrinhoService);
 
     formulario = new FormGroup({
-      nome: new FormControl(''),
-      email: new FormControl(''),
-      endereco: new FormControl(''),
-    });
+    nome: new FormControl('', [Validators.required, Validators.minLength(3), nomeSemNumeros]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    endereco: new FormControl('', [Validators.required, Validators.minLength(5)]),
+  });
 
-    finalizar() {
-      console.log('Dados do formulário:', this.formulario.value);
-      console.log('Itens do carrinho:', this.carrinhoService.itens());
+     finalizar() {
+    if (this.formulario.invalid) {
+      console.log('Formulário inválido');
+      return;
     }
+
+    const dados = this.formulario.value;
+    const itens = this.carrinhoService.itens();
+
+    console.log('Dados do formulário:', dados);
+    console.log('Itens do carrinho:', itens);
+  }
+}
+
+  function nomeSemNumeros(control: AbstractControl): ValidationErrors | null {
+
+    const valor = control.value;
+
+    if (!valor) return null;
+
+    if (/\d/.test(valor)) {
+      return { numeroInvalido: true };
+    }
+
+    return null;
   }
